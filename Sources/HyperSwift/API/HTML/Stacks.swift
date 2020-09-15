9dd309547bec2e495052c85fe00ae98b5381bece
John Garrett <jacgarrett18@gmail.com>
2020-09-15 14:37:29 -0400

    fix initalzer issue with swift 5.3

--- Sources/HyperSwift/API/HTML/Stacks.swift
+++ Sources/HyperSwift/API/HTML/Stacks.swift
@@ -3,39 +3,35 @@ import Foundation
 public class HStack: HTMLComponent { }
 public extension HStack {
     convenience init(
-        _ cls: String? = nil,
+        _ cssClass: String = "",
         justify justification: Justification = .flexStart,
         align alignment: Alignment = .stretch,
         wrap: FlexWrap = .nowrap,
         @HStackBuilder _ children: () -> [HTMLElement]
     ) {
         self.init(
-            Div(cls ?? "g_hstack_\(justification.rawValue)_\(alignment.rawValue)_\(wrap.rawValue)") {
-                children()
-            }
-            .display(.flex)
-            .justifyContent(justification)
-            .alignItems(alignment)
-            .flexDirection(.row)
-            .flexWrap(wrap)
+            Div(cssClass, children: children())
+                .display(.flex)
+                .justifyContent(justification)
+                .alignItems(alignment)
+                .flexDirection(.row)
+                .flexWrap(wrap)
         )
     }
     convenience init(
-        _ cls: String? = nil,
+        _ cssClass: String = "",
         justify justification: Justification = .flexStart,
         align alignment: Alignment = .stretch,
         wrap: FlexWrap = .nowrap,
         @HStackBuilder _ child: () -> HTMLElement
     ) {
         self.init(
-            Div(cls ?? "g_hstack_\(justification.rawValue)_\(alignment.rawValue)_\(wrap.rawValue)") {
-                [child()]
-            }
-            .display(.flex)
-            .justifyContent(justification)
-            .alignItems(alignment)
-            .flexDirection(.row)
-            .flexWrap(wrap)
+            Div(cssClass, children: [child()])
+                .display(.flex)
+                .justifyContent(justification)
+                .alignItems(alignment)
+                .flexDirection(.row)
+                .flexWrap(wrap)
         )
     }
 }
@@ -51,39 +47,35 @@ public class VStack: HTMLComponent { }
 
 public extension VStack {
     convenience init(
-        _ cls: String? = nil,
+        _ cssClass: String = "",
         justify justification: Justification = .flexStart,
         align alignment: Alignment = .stretch,
         wrap: FlexWrap = .nowrap,
         @VStackBuilder _ children: () -> [HTMLElement]
     ) {
         self.init(
-            Div(cls ?? "g_vstack_\(justification.rawValue)_\(alignment.rawValue)_\(wrap.rawValue)") {
-                children()
-            }
-            .display(.flex)
-            .justifyContent(justification)
-            .alignItems(alignment)
-            .flexDirection(.column)
-            .flexWrap(wrap)
+            Div(cssClass, children: children())
+                .display(.flex)
+                .justifyContent(justification)
+                .alignItems(alignment)
+                .flexDirection(.column)
+                .flexWrap(wrap)
         )
     }
     convenience init(
-        _ cls: String? = nil,
+        _ cssClass: String = "",
         justify justification: Justification = .flexStart,
         align alignment: Alignment = .stretch,
         wrap: FlexWrap = .nowrap,
         @VStackBuilder _ child: () -> HTMLElement
     ) {
         self.init(
-            Div(cls ?? "g_vstack_\(justification.rawValue)_\(alignment.rawValue)_\(wrap.rawValue)") {
-                [child()]
-            }
-            .display(.flex)
-            .justifyContent(justification)
-            .alignItems(alignment)
-            .flexDirection(.column)
-            .flexWrap(wrap)
+            Div(cssClass, children: [child()])
+                .display(.flex)
+                .justifyContent(justification)
+                .alignItems(alignment)
+                .flexDirection(.column)
+                .flexWrap(wrap)
         )
     }
 }
