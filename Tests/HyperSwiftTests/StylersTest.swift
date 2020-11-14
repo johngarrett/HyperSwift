de7618a761e63e32317ffbce0b29a4b36cdb78cd
John Garrett <jacgarrett18@gmail.com>
2020-11-14 12:20:19 -0500

    start stylers test

--- Tests/HyperSwiftTests/StylersTest.swift
+++ Tests/HyperSwiftTests/StylersTest.swift
@@ -1,6 +1,100 @@
 import XCTest
+/*
 import HyperSwift
 
 final class StylersTest: XCTestCase {
+    let v: Double = 1
+    let color = CSSColor("white")
+    let base = Div { }
+    func a() {
+        let t = Div {
+            
+        }.position(top: v, left: v)
+        .zIndex(v)
+        .color(color)
+        .shadow(x: v, y: v, spread: v, blur: v, color: color)
+        .wordWrap(.breakWord)
+        .overflow(.scroll)
+        .whiteSpace(.normal)
+        .textAlign(.left)
+        .textDecoration(.none)
+        .backgroundColor(color)
+    }
     
+    func testBorder() {
+        base.border(v, .dashed, color: color)
+        base.borderRight(v, .dashed, color: color)
+        base.borderLeft(v, .dashed, color: color)
+        base.borderTop(v, .dashed, color: color)
+        base.borderBottom(v, .dashed, color: color)
+        base.borderRadius(v)
+    }
+
 }
+
+func display(_ dt: DisplayType) -> HTMLComponent {
+    updateComponent(with: "display: \(dt.rawValue);")
+}
+func justifyContent(_ jst: Justification) -> HTMLComponent {
+    updateComponent(with: "justify-content: \(jst.rawValue);")
+}
+func alignItems(_ alignment: Alignment) -> HTMLComponent {
+    updateComponent(with: "align-items: \(alignment.rawValue);")
+}
+func flexDirection(_ direction: FlexDirection) -> HTMLComponent {
+    updateComponent(with: "flex-direction: \(direction.rawValue);")
+}
+func flexWrap(_ wrap: FlexWrap) -> HTMLComponent {
+    updateComponent(with: "flex-wrap: \(wrap.rawValue);")
+}
+
+func gridGap(_ space: Double, _ type: CSSUnit.UnitType = .px) -> HTMLComponent {
+    updateComponent(with: "grid-gap: \(CSSUnit(space, type));")
+}
+func font(weight: FontWeight, size: Double, family: String) -> HTMLComponent {
+    updateComponent(with: "font-weight:\(weight.rawValue); font-size:\(size)px; font-family:\(family);")
+}
+
+func font(weight: String, size: Double, family: String) -> HTMLComponent {
+    updateComponent(with: "font-weight:\(weight); font-size:\(size)px; font-family:\(family);")
+}
+func width(_ width: Double, _ type: CSSUnit.UnitType = .px) -> HTMLComponent {
+    updateComponent(with: "width:\(CSSUnit(width, type));")
+}
+func height(_ height: Double, _ type: CSSUnit.UnitType = .px) -> HTMLComponent {
+    updateComponent(with: "height:\(CSSUnit(height, type));")
+}
+
+func maxWidth(_ width: Double, _ type: CSSUnit.UnitType = .px) -> HTMLComponent {
+    updateComponent(with: "max-width:\(CSSUnit(width, type));")
+}
+func minWidth(_ width: Double, _ type: CSSUnit.UnitType = .px) -> HTMLComponent {
+    updateComponent(with: "min-width:\(CSSUnit(width, type));")
+}
+func maxHeight(_ height: Double, _ type: CSSUnit.UnitType = .px) -> HTMLComponent {
+    updateComponent(with: "max-height:\(CSSUnit(height, type));")
+}
+func minHeight(_ height: Double, _ type: CSSUnit.UnitType = .px) -> HTMLComponent {
+    updateComponent(with: "min-height:\(CSSUnit(height, type));")
+}
+func padding(
+    top: Double=0, right: Double=0, bottom: Double=0, left: Double=0,
+    _ type: CSSUnit.UnitType = .px
+) -> HTMLComponent {
+    updateComponent(with: "padding:\(CSSUnit(top, type)) \(CSSUnit(right, type)) \(CSSUnit(bottom, type)) \(CSSUnit(left, type));")
+}
+func padding(
+    _ value: Double=0,
+    _ direction: FrameDirection,
+    _ type: CSSUnit.UnitType = .px
+) -> HTMLComponent {
+    switch direction {
+    case .all:
+        return updateComponent(with: "padding: \(CSSUnit(value, type));")
+    case .horizontal:
+        return updateComponent(with: "padding: inital \(CSSUnit(value, type));")
+    case .vertical:
+        return updateComponent(with: "padding: \(CSSUnit(value, type)) inital;")
+    }
+}
+ */
